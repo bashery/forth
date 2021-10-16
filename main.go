fea2d11eafdaa6ea675ddb8da3eb575954258624
bashery <ahmedalbachiry@gmail.com>
2021-10-16 13:45:29 +0300

    forth language

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Forth programing language, writen in go")
+	//fmt.Println("Forth programing language, writen in go")
+	//num := os.Args
+
+	t := 20
+	for i := 0; i < 10; i++ {
+		t += ((t / 100) * 20) + 20
+	}
+
+	fmt.Println(t)
 
 }
